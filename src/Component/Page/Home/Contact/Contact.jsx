f7fe1a987bfe3342f544f9c76de98a67a892fad3
tasniaarhamarham <tasniaarhamarham@gmail.com>
2023-08-05 22:47:09 +0600

    contact_pagedone

--- src/Component/Page/Home/Contact/Contact.jsx
+++ src/Component/Page/Home/Contact/Contact.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import banner from '../../../../assets/img/banner/b1.jpg'
+import people1 from '../../../../assets/img/people/1.png';
+import people2 from '../../../../assets/img/people/2.png'
+import people3 from '../../../../assets/img/people/3.png'
 
 const Contact = () => {
     return (
@@ -16,7 +19,7 @@ const Contact = () => {
 </div>
 
 
-<div className='lg:px-20 py-20 px-5 flex lg:flex-row flex-col justify-between items-start'>
+<div className='lg:px-20 py-20 px-5 flex lg:flex-row flex-col lg:justify-between '>
 
 <div className='mb-6 lg:mb-0'>
 <h1 className='text-gray-500'>GET IN TOUCH</h1>
@@ -31,10 +34,10 @@ const Contact = () => {
 
 </div>
 
-<div className='map'>
+<div className='map lg:w-1/2 w-full'>
 
 
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29413.738633290155!2d89.51502481251143!3d22.84994527294484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ff9a8e9e2b4f4b%3A0xc23137d9910ecc0b!2sKhalishpur!5e0!3m2!1sen!2sbd!4v1691234435577!5m2!1sen!2sbd" width="600" height="450"  allowfullscreen="" loading="lazy"  style={{border:"0",referrerpolicy:"no-referrer-when-downgrade"}}></iframe>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29413.738633290155!2d89.51502481251143!3d22.84994527294484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ff9a8e9e2b4f4b%3A0xc23137d9910ecc0b!2sKhalishpur!5e0!3m2!1sen!2sbd!4v1691234435577!5m2!1sen!2sbd"className='w-full' height="450"  allowfullscreen="" loading="lazy"  style={{border:"0",referrerpolicy:"no-referrer-when-downgrade"}}></iframe>
 </div>
 
 
@@ -42,7 +45,7 @@ const Contact = () => {
 </div>
 
 
-<div className='lg:px-20 py-20 px-5 flex lg:flex-row flex-col justify-between items-center'>
+<div className='lg:px-20 py-20 px-5 flex lg:flex-row flex-col justify-between items-start'>
     <div className='w-full lg:w-1/2'>
         <div className='text-gray-500'>LEAVE A MESSAGE</div>
         <p className='lg:text-3xl my-2 text-xl font-bold'style={{color:"#1a1a1a"}}>We love to hear from you</p>
@@ -58,6 +61,33 @@ const Contact = () => {
 </form>
 
     </div>
+
+    <div className='grid lg:grid-cols-1 grid-cols-1 md:grid-cols-2 gap-3 w-full lg:w-1/2'>
+
+    <div className='flex justify-center  gap-3 p-4'>
+        <div><img src={people1} alt="people"className='h-[80px]'/></div>
+        <div><p className='font-bold'>John Doe</p><p className='text-gray-500'>Senior Marketing Manager</p><p className='text-gray-500'>Phone:<span className='text-gray-500 ms-2'>+88124343423</span></p><p className='text-gray-500'>Email: <span className='text-gray-500 ms-2'>doe@gmail.com</span></p></div>
+    </div>
+
+
+    <div className='flex justify-center  gap-3 p-4'>
+        <div><img src={people2} alt="people"className='h-[80px]'/></div>
+        <div><p className='font-bold'>Wiliam Smith</p><p className='text-gray-500'>Senior Marketing Manager</p><p className='text-gray-500'>Phone:<span className='text-gray-500 ms-2'>+88124343423</span></p><p className='text-gray-500'>Email: <span className='text-gray-500 ms-2'>smith@gmail.com</span></p></div>
+    </div>
+
+
+    <div className='flex justify-center gap-3 p-4'>
+        <div><img src={people3} alt="people"className='h-[80px]'/></div>
+        <div><p className='font-bold'>Emma Stone</p><p className='text-gray-500'>Senior Marketing Manager</p><p className='text-gray-500'>Phone:<span className='text-gray-500 ms-2'>+88124343423</span></p><p className='text-gray-500'>Email: <span className='text-gray-500 ms-2'>emma@gmail.com</span></p></div>
+    </div>
+    
+    
+    </div>
+
+
+
+
+
 
 </div>
 
